90f4efb0e551cd5595f7e14c6b998c56728fe724
Will Hall <willhall@outlook.com>
2021-07-31 12:54:35 -0700

    Add assignment files provided by Dr. Stu Steiner

---
+++ CSCD437RegExMethods.java
@@ -0,0 +1,121 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CSCD437RegExMethods
+{
+	public static void easySSNPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+         				
+	}// end easySSN
+   
+   
+   public static void mediumEasySSNPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+				
+	}// end mediumEasySSN
+   
+   
+   public static void newSSNPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+				
+	}// end newSSN
+
+
+   public static void phonePatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+  
+	}// end phoneNumber
+
+
+	public static void emailPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end email
+   
+   
+	public static void namePatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+   
+	}// end name
+	
+   
+   public static void datePatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end date
+	
+   
+   public static void addressPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end address
+	
+   
+   public static void cityStateZipPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}//end cityStateZip
+	
+   
+   public static void militaryTimePatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end militaryTime
+	
+   
+   public static void moneyPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end money
+	
+   
+   public static void urlPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end URL
+	
+   
+   public static void passwordPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end password
+	
+   
+   public static void evenErPatternTester(final String str, final PrintStream fout)
+	{
+      if(str == null || str.isEmpty())
+         throw new IllegalArgumentException("Bad str Parameter to the method");
+
+	}// end oddIon
+
+}// end class
